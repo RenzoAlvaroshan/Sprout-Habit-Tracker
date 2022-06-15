ee3fdb7d49b0df0c874aa4e83ec4be9360c368a5
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-06-15 09:54:32 +0700

    Add RewardController and TaskController

---
+++ MC2/Controller/Tabs/RewardController.swift
@@ -0,0 +1,16 @@
+//
+//  RewardController.swift
+//  MC2
+//
+//  Created by Stephen Giovanni Saputra on 15/06/22.
+//
+
+import UIKit
+
+class RewardController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+}
---
+++ MC2/Controller/Tabs/TaskController.swift
@@ -0,0 +1,16 @@
+//
+//  TaskController.swift
+//  MC2
+//
+//  Created by Stephen Giovanni Saputra on 15/06/22.
+//
+
+import UIKit
+
+class TaskController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
