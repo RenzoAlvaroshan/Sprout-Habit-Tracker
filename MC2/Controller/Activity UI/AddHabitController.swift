bb168aaad451ce5b5f55ae05e81456de94267e6b
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-06-24 10:26:09 +0700

    COK

--- MC2/Controller/Activity UI/AddHabitController.swift
+++ MC2/Controller/Activity UI/AddHabitController.swift
@@ -334,7 +334,7 @@ class AddHabitController: UIViewController {
     }
     
     @objc func handleAddGoal() {
-        print(habitButton01.titleLabel?.text)
+        print("COK")
         
     }
     
